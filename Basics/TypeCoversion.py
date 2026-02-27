6706c85bff625215e9821f8bcbef262623521ec0
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-02-27 20:21:50 +0530

    Added program based on Type Conversion

---
+++ Basics/TypeCoversion.py
@@ -0,0 +1,6 @@
+#type conversion means converting the data type of a variable
+
+age ="45"              #it stores 45 as a string
+print(int(age))        #here we have converted age into int 
+print(int(age)+5)      #here we have converted age into int so that we will get 50 as o/p
+print(age+5)           #age is string so that we can't add
