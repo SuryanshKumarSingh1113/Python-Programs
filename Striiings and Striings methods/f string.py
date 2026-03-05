3f96086602ae42bc11da7f5e901de2f71df7442f
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-03-05 15:11:40 +0530

    Added how to use f-strings

---
+++ Striiings and Striings methods/f string.py
@@ -0,0 +1,9 @@
+#f-strings (formatted string literals) are an easy and modern way to format strings in Python. They were introduced in Python 3.6.
+
+# To use an f-string, you add an f before the string and place variables or expressions inside {}.
+
+name = input("Enter your name :")
+age = input("Enter  your age :")
+#print("I'm", name ,"and I'm",age,"years old")      Instead of writing this we can use f strings
+
+print(f"My name is {name} and i'm {age} years old")
