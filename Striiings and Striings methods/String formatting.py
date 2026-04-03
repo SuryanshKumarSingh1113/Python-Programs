4462211b073b22df29d38fed4acc3806d7cf4af9
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-03 05:38:17 +0530

    Added string formatting related program

---
+++ Striiings and Striings methods/String formatting.py
@@ -0,0 +1,14 @@
+# String formatting allows inserting values into strings
+name = "Suryansh kumar"
+print(f"My name is {name} singh")           #using f string
+age = 21
+print(f"I'm {name} singh and I'm {age} years old")
+
+# Checking String Content
+# Python provides methods to check string content.
+
+text = "Python123"
+
+print(text.isalpha())
+print(text.isdigit())
+print(text.isalnum())
