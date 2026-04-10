1fb6ebc5dc343cdcb63843068f0acc851068331e
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-10 21:25:16 +0530

    Concept of packing and unpacking in tuples

---
+++ Tuples and Tuples Methods/Tuples packing and unpacking.py
@@ -0,0 +1,16 @@
+                                                    # Tuple Packing and Unpacking
+
+
+# Packing
+# Multiple values can be packed into a tuple.
+data = 10, 20, 30
+print(data)
+
+
+# Unpacking
+# Tuple values can be unpacked into separate variables.
+
+a, b, c = data
+print(c)
+print(a)
+print(b)
