842b35a56a0ddb229b7ceffc7f8497d327ce24e4
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-17 08:41:19 +0530

    It includes keys(),values(),items() methods of dictionary

---
+++ Dictonaries/Dictionary keys,values,items.py
@@ -0,0 +1,22 @@
+player = {
+    "name":"Cristiano Ronaldo",
+     "age": 41,
+     "club": "Real Madrid"
+}
+
+
+
+# keys()
+# Returns all keys.
+
+print(player.keys())
+
+# values()
+# Returns all values.
+
+print(player.values())
+
+# items()
+# Returns key value pairs as tuples.
+
+print(player.items())
