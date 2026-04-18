cdf144cc02c2b7f77c9be9a2ae72b2760c537df4
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-18 10:55:57 +0530

    Using if else statements in python

---
+++ Conditional Statements/if else statements.py
@@ -0,0 +1,22 @@
+# The else Statement
+# The else block runs when the if condition is false.
+
+
+age = 16
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+
+
+
+# Using pass in Conditionals
+# The pass statement is used when a condition is required syntactically but no action is needed or when we want to write if condition after writing else
+
+age = 15
+ 
+if age >= 18:
+    pass
+else:
+    print("Minor")
