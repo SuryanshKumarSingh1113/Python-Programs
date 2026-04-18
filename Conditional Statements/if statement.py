884a087fb5958d438b78f02fdcae83ca505ad4a3
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-18 10:54:31 +0530

    Using if statement in python

---
+++ Conditional Statements/if statement.py
@@ -0,0 +1,32 @@
+                                                # Conditional Statements in Python
+# Conditional statements are used to make decisions in a program.
+# They allow the program to execute different blocks of code based on conditions.
+# Python uses if, elif, and else for conditional logic.
+
+
+# The if Statement
+
+# The if statement checks a condition.
+# If the condition is true, the code inside the block runs.
+ 
+age = 18
+if age >= 18:
+    print("Eligible")
+
+
+a=int(input("Enter a number "))
+if a>40:
+    print("a is greater than 40")
+
+
+
+# Indentation in Conditionals
+# Python uses indentation to define code blocks.
+# Incorrect indentation(spacing) will cause an error.
+
+if True:
+    print("This runs")
+    print("This also runs")
+
+
+ 
