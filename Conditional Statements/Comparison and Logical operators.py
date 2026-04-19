a3062f2631606cfd9852e17d030ac660628b9ba1
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-19 19:19:09 +0530

    Comparison and logical operators

---
+++ Conditional Statements/Comparison and Logical operators.py
@@ -0,0 +1,20 @@
+# Comparison Operators in Conditions
+# Conditions often use comparison operators.
+
+x = 10
+ 
+if x == 10:
+    print("Equal")
+ 
+if x != 5:
+    print("Not equal")
+
+# Logical Operators in Conditions
+# Logical operators allow combining multiple conditions.
+
+age = 25
+country = "India"
+ 
+if age >= 18 and country == "India":
+    print("Allowed")
+
