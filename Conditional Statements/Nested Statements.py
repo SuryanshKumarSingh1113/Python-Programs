c6784bd05cc0ee100768d3ad65c32db18dc36aa3
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-19 19:20:08 +0530

    Nested statements

---
+++ Conditional Statements/Nested Statements.py
@@ -0,0 +1,11 @@
+# Nested Conditionals
+# Conditionals can be placed inside other conditionals.
+
+age = 65
+ 
+if age >= 18:
+    if age < 60:
+        print("Adult")
+else:
+    print("Minor")        
+    
