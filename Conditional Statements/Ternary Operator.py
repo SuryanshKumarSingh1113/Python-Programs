1f692dd470378937a57b3b4c3ebd40b430ff2fcd
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-19 19:21:41 +0530

    Ternary operators

---
+++ Conditional Statements/Ternary Operator.py
@@ -0,0 +1,8 @@
+                                    # Conditional Expressions (Ternary Operator)
+#Python supports one line conditionals.
+
+age = 20
+status = "Adult" if age >= 18 else "Minor"
+print(status)
+
+
