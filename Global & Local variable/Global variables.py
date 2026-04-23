a138e02af09f44b894c365bffebfb2d5c3cbb980
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-23 19:13:49 +0530

    Implemented Global variables example

---
+++ Global & Local variable/Global variables.py
@@ -0,0 +1,12 @@
+# Global Variables
+# Global variables are created outside all functions. They can be accessed from anywhere in the program.
+
+x = 20
+ 
+def show_value():
+    print(x)
+ 
+show_value()
+
+# Here, x is a global variable.
+
