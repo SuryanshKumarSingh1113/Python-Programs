ced76773c77ae91ae48584c5bbb195c2b92ff6f4
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-23 19:16:47 +0530

    Implemented Local Variables examples

---
+++ Global & Local variable/Local variables.py
@@ -0,0 +1,15 @@
+# Local Variables
+# Local variables are created inside a function.
+# They can be used only within that function.
+
+def show_value():
+    x = 10
+    print(x)
+ 
+show_value()
+
+# Here, x is a local variable.
+# If you try to access it outside the function, it will cause an error.
+
+print(x)
+
