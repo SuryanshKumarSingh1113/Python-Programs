752a8fae81e5254d9f8bb4ebfcc50fcf3c08357b
Suryansh Kumar Singh <singh.suryanshwork@gmail.com>
2026-04-28 22:59:52 +0530

    It checks whether a file exists or not

---
+++ File Handling/Checking file existence.py
@@ -0,0 +1,10 @@
+# Checking File Existence
+import os
+ 
+if os.path.exists("data2.txt"):
+    print("File exists")
+
+if os.path.exists("data3.txt"):
+    print("File exists")  
+else:
+    print("not exits")  
